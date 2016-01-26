f07582da79ee98cf218013746835d1c8d0c008a3
Sumalah <adam.halamus@gmail.com>
2016-01-26 12:16:01 +0100

    Initial commit

---
+++ src/com/gametutorial/main/Game.java
@@ -0,0 +1,20 @@
+package com.gametutorial.main;
+
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable{
+    private static final long serialVersionUID = 6039990521162748952L;
+
+    public synchronized  void start(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
